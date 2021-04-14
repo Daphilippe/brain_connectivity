8154abc2a19f57bac7005c619947c6a6e3863227
Daphilippe <58039594+Daphilippe@users.noreply.github.com>
2021-04-15 00:26:44 +0200

    Barycentre

---
+++ simulation_4/main_barycenter.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Duy Anh Philippe Pham
+@date: 15/04/2021
+@version: 1.0
+@Recommandation: Python 3.7
+@revision: 14/04/2021
+@But: Barycentre
+"""
+import numpy as np
+import ot
+
+import tools
+from scipy.spatial.distance import sqeuclidean
+
+import glob
+import sys
+
+numpy_vars = {}
+for np_name in glob.glob('./data/L/*.np[yz]'):
+    numpy_vars[np_name] = np.load(np_name)
+
+Lval=[]
+Lname=[]
+for i in numpy_vars:
+    L2=[]
+    for j in numpy_vars:
+        if i==j:
+            continue
+        else:
+            # xs = numpy_vars[i]
+            # a = ot.emd(ot.unif(len(numpy_vars[i]))
+            # xt = numpy_vars[j]
+            # b = ot.emd(ot.unif(len(numpy_vars[j]))
+            # M = ot.dist(xs,xt)
+            # G = ot.emd(a,b,M, numItermax=1000000))
+            # pos1_G,_= tools.degree(xs,xt,G,degree=1)
+            pos1_G,_=tools.degree(numpy_vars[i],numpy_vars[j],ot.emd(ot.unif(len(numpy_vars[i])), ot.unif(len(numpy_vars[j])), ot.dist(numpy_vars[i],numpy_vars[j]), numItermax=1000000),degree=1)
+            # On déterminer la distance entre les points utilisés dans la correspondance entre la source i et la cible j
+            # On additionne l'ensemble des distances nécessaire
+            L2.append(np.sum(  [sqeuclidean(numpy_vars[i][pos1_G[x,1]],numpy_vars[j][pos1_G[x,0]]) for x in range(np.shape(pos1_G)[0])] ))
+    Lval.append(np.sum(L2))
+    Lname.append(i)
+argmin=np.argmin(Lval)
+barycentre=Lname[argmin]
+print(barycentre)
+
+sys.exit()
--- simulation_4/main_real.py
+++ simulation_4/main_real.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 @author: Duy Anh Philippe Pham
-@date: 08/04/2021
+@date: 14/04/2021
 @version: 1.0
 @Recommandation: Python 3.7
-@But: Données réelles
+@revision: 14/04/2021
+@But: Données réelles / mesocentre
 """
 import numpy as np
 import ot
@@ -13,28 +14,46 @@ import tools
 import process
 import sys
 
-racine_data='data/'    
-sujet='100206_L_connectivity_withHKnob.npy'
-xs=np.load(racine_data+sujet)
-a=ot.unif(len(xs))
-sujet='103111_L_connectivity_withHKnob.npy'
-xt=np.load(racine_data+sujet)
-b=ot.unif(len(xt))
+racine_data='data/L'    
+sujet1='100206_L_connectivity_withHKnob.npy'
+sujet2='103111_L_connectivity_withHKnob.npy'
 
 # Discret
-M = ot.dist(xs,xt)
-G = ot.emd(a, b, M, numItermax=1000000)
-
-process.automatisation_discret(xs,a,xt,b,M,G,'test','G')
-
+xs=np.load(racine_data+sujet1)
+a=ot.unif(len(xs))
+xt=np.load(racine_data+sujet2)
+b=ot.unif(len(xt))
 # Continue
 _,_,Img_Xs=tools.estimate_pseudo_density(xs)
 Img2Xs,aa=tools.extract_point(Img_Xs)
 _,_,Img_Xt=tools.estimate_pseudo_density(xt)
 Img2Xt,bb=tools.extract_point(Img_Xt)
 
+racine='test'
+Md = ot.dist(xs,xt)
 Mc = ot.dist(Img2Xs, Img2Xt)
-Gc = ot.emd(aa, bb, Mc, numItermax=1000000)
+if False:
+    # Discret
+    G = ot.emd(a, b, Md, numItermax=1000000)
+    process.automatisation_discret(xs,a,xt,b,Md,G,racine,'G')
+    
+    # Continue    
+    Gc = ot.emd(aa, bb, Mc, numItermax=1000000)
+    process.automatisation_continue(Img2Xs,aa,Img2Xt,bb,Mc,Gc,racine,'Gc')
 
-process.automatisation_continue(Img2Xs,aa,Img2Xt,bb,Mc,Gc,'test','Gc')
+if False:
+    print('Regularisation entropique')
+    def f(G):
+        #Frobeniusregularization
+        return 0.5 * np.sum(G**2)
+    
+    def df(G):
+        return G
+    
+    reg_lasso=10
+    reg_entropique=10
+    temp2=ot.optim.gcg(aa, bb, Mc, reg_entropique, reg_lasso, f, df)
+        
+    affixe= '2reg_'+str(reg_entropique)+'_'+str(reg_lasso)
+    process.automatisation_continue(Img2Xs,aa,Img2Xt,bb,Mc,temp2,racine,affixe)
 sys.exit()
