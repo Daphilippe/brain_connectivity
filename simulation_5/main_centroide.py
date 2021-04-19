92a3e86eb2123fb2e6d19860cccc0789616a3b1c
Daphilippe <58039594+Daphilippe@users.noreply.github.com>
2021-04-19 16:35:53 +0200

    Correctif

--- simulation_5/main_centroide.py
+++ simulation_5/main_centroide.py
@@ -19,34 +19,35 @@ import sys
 
 print('Begin')
 numpy_vars = {}
-for np_name in glob.glob('./data/L/*.np[yz]'):
+for np_name in glob.glob('./data/R/*.np[yz]'):
     numpy_vars[np_name] = np.load(np_name)
 
 Lval=[]
 Lname=[]
 for i in numpy_vars:
-    L2=[]
-    tic = time.clock()
+    L2_val=[]
+    L2_name=[]
+    #tic = time.clock()
     for j in numpy_vars:
-        if i==j:
-            continue
-        else:
-            # xs = numpy_vars[i]
-            # a = ot.emd(ot.unif(len(numpy_vars[i]))
-            # xt = numpy_vars[j]
-            # b = ot.emd(ot.unif(len(numpy_vars[j]))
-            # M = ot.dist(xs,xt)
-            # G = ot.emd(a,b,M, numItermax=1000000))
-            # pos1_G,_= tools.degree(xs,xt,G,degree=1) # \Sigma(w_ij*d_ij) #implémenter ça (cf calcul matriciel)
-            M=ot.dist(numpy_vars[i],numpy_vars[j])
-            G=ot.emd(ot.unif(len(numpy_vars[i])), ot.unif(len(numpy_vars[j])),M,numItermax=1000000)
-            tools.save_value(G,'G_emd_'+str(j[9:17]),directory='variables/'+str(i[9:17]))
-            cost=G*M       
-            L2.append(np.sum(cost))# mettre au carrée
-    Lval.append(np.sum(L2))
+        # xs = numpy_vars[i]
+        # a = ot.emd(ot.unif(len(numpy_vars[i]))
+        # xt = numpy_vars[j]
+        # b = ot.emd(ot.unif(len(numpy_vars[j]))
+        # M = ot.dist(xs,xt)
+        # G = ot.emd(a,b,M, numItermax=1000000))
+        # pos1_G,_= tools.degree(xs,xt,G,degree=1) # \Sigma(w_ij*d_ij) #implémenter ça (cf calcul matriciel)
+        M=ot.dist(numpy_vars[i],numpy_vars[j])
+        G=ot.emd(ot.unif(len(numpy_vars[i])), ot.unif(len(numpy_vars[j])),M,numItermax=1000000)
+        #tools.save_value(G,'G_emd_'+str(j[9:17]),directory='variables/'+str(i[9:17]))#attention à la taille
+        cost=G*M    
+        L2_name.append(str(j[9:17]))
+        L2_val.append(np.sum(cost))# mettre au carrée
+    tools.save_value(L2_val,'L2_val_'+str(i[9:17]),directory='variables/'+str(i[9:17]))
+    tools.save_value(L2_name,'L2_name_'+str(i[9:17]),directory='variables/'+str(i[9:17]))
+    Lval.append(np.sum(L2_val))
     Lname.append(i)
-    toc = time.clock()
-    print('\n Time: ',toc-tic)
+    #toc = time.clock()
+    #print('\n Time: ',toc-tic)
 argmin=np.argmin(Lval)
 centroide=Lname[argmin]
 print(centroide)
