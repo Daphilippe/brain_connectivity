993978fdd66d6d992d75ee8e0813c86d9e1f747b
Daphilippe <58039594+Daphilippe@users.noreply.github.com>
2021-04-27 14:01:04 +0200

    270421.1401

--- test/main_barycentre.py
+++ test/main_barycentre.py
@@ -23,14 +23,10 @@ destination='test/barycentre/'
 # Changement des données
 measures_locations = []
 measures_weights = []
-N=1
-Nmax=100
 for np_name in glob.glob(str(source)+'*.np[yz]'):
     measures_locations.append(np.load(np_name))
     measures_weights.append(ot.unif(len(measures_locations[-1])))
-    if N>=Nmax:
-        break
-    N=N+1
+Nmax=np.shape(measures_locations)[0]
 
 # Initialisation du profil type
 LX=[int(i[len(destination):-4]) for i in glob.glob(str(destination)+'*.np[yz]') ]
@@ -41,20 +37,38 @@ if len(LX)!=0:
     b=ot.unif(np.shape(X)[0])
 else:
     # Création du profil type
+    itermax=0
     X_init = np.load('.'+str(np.load(variables+'centroide.npy')).replace('\\','/'))  # centroide
     b=ot.unif(np.shape(X_init)[0])
     X=None
 
-# Calcul du barycentre 1 
-for i in range(0,100):
-    if X is None:
-        X = ot.lp.free_support_barycenter(measures_locations, measures_weights,X_init,b,numItermax=1)
-    else:
-        X_init=X
-        X = ot.lp.free_support_barycenter(measures_locations, measures_weights,X_init,b,numItermax=1)
-        
-    # Sauvegarde
-    tools.save_value(X,str(i+itermax+1),directory=destination)
-    display.show_dot(X,title='Barycenter')
-    tools.save_fig(str(i+itermax+1),directory=destination)
+# Calcul du barycentre sur l'ensemble des sujets et on sauvegarde chaque étape de la convergence
+if False:
+    for i in range(0,100):
+        if X is None:
+            X = ot.lp.free_support_barycenter(measures_locations, measures_weights,X_init,b,numItermax=1)
+        else:
+            X_init=X
+            X = ot.lp.free_support_barycenter(measures_locations, measures_weights,X_init,b,numItermax=1)
+            
+        # Sauvegarde
+        tools.save_value(X,str(i+itermax+1),directory=destination)
+        display.show_dot(X,title='Barycenter')
+        tools.save_fig(str(i+itermax+1),directory=destination)
+
+# Calcule du barycentre avec une convergence acceptable sujet par sujet : de manière itérative        
+if True:
+    for i in range(1,Nmax-itermax):# On calcule 99 barycentres si on a 100 sujets
+        if X is None:
+            # On prend les 2 premiers sujets de la liste position 0 et 1
+            X = ot.lp.free_support_barycenter(measures_locations[:2], measures_weights[:2],X_init,b,numItermax=1000)
+        else:
+            X_init=X
+            L_loc=[X_init,measures_locations[i+itermax]]#sujet suivant pour calcul du barycentre position 2
+            L_w=[b,measures_weights[i+itermax]]
+            X = ot.lp.free_support_barycenter(L_loc, L_w,X_init,b,weights=np.array([(i+itermax)/(i+itermax+1),1/(i+itermax+1)]),numItermax=1000)  
+        # Sauvegarde
+        tools.save_value(X,str(i+itermax),directory=destination)
+        display.show_dot(X,title='Barycenter')
+        tools.save_fig(str(i+itermax),directory=destination)
 sys.exit()
