d5f91d5116e2979dc7e1272c61fe2a676a91af48
Daphilippe <58039594+Daphilippe@users.noreply.github.com>
2021-04-28 13:45:50 +0200

    280421.1345

--- test/test.py
+++ test/test.py
@@ -65,4 +65,10 @@ if False:
         display.show_dot(X,title='Barycenter')
         tools.save_fig(str(i+itermax),directory=destination)
 """
+import display,tools
+X=np.load('../variables/L/barycentre.npy')
+display.show_dot(X,title='Barycenter - dots')
+
+_,_,Img_xs=tools.estimate_pseudo_density(X)
+display.show_map(Img_xs,title='Barycenter - map')
 sys.exit()
