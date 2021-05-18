2e9358592690fde4085a5317f108ad17bba6cb5d
Daphilippe <58039594+Daphilippe@users.noreply.github.com>
2021-05-18 09:34:41 +0200

    180521.0934

--- test/main_clustering.py
+++ test/main_clustering.py
@@ -13,17 +13,17 @@ import sys
 import pandas as pd
 import matplotlib.pylab as plt
 
-# Left
-d=np.load('../variables/L/matrix.npy')
-index=np.load('../variables/L/matrix_index.npy',allow_pickle=True)
-columns=np.load('../variables/L/matrix_columns.npy',allow_pickle=True)
+hemi='L'
+d=np.load('../variables/'+hemi+'/matrix.npy')
+index=np.load('../variables/'+hemi+'/matrix_index.npy',allow_pickle=True)
+columns=np.load('../variables/'+hemi+'/matrix_columns.npy',allow_pickle=True)
 data=pd.DataFrame((d>0.01)*d,index=index,columns=columns)#0 sur la diagonale, probleme de virgule flotante résolue
 
 # diagonalisation
 if True: #analyse spectrale
     from numpy import linalg as LA
     df=data.copy()
-    q, eig_v = LA.eig(d)# on a np.dot(np.dot(v,np.diag(w)),v.transpose())=d
+    q, eig_v = LA.eig(np.sqrt(d))# on a np.dot(np.dot(v,np.diag(w)),v.transpose())=d
     # trie croisant
     eigv_or=np.fliplr(eig_v)
     q_or=q[::-1]
@@ -44,18 +44,17 @@ if True: #analyse spectrale
     plt.show()
     print(p)
 
-if True:# clustering hierarchique
+if False:# clustering hierarchique
     import scipy.cluster.hierarchy as sch
     df=data.copy()
-    #sim = np.exp(- 0.5*(d /np.max(d))**2) # pour obtenir un similarity matrix
-    sim=1-d/np.max(d)
-    L = sch.linkage(sim, method='weighted')#alias WPGMA --complete,centroid,average,weighted
-    plt.figure()
-    sch.dendrogram(L)
-    plt.show()
+    dist=sch.ward(np.sqrt(df))
     
     cluster=100#np.shape(d)[0]
-    label = sch.fcluster(L,cluster,'maxclust') # or distance avec un seuille de différence
+    label = sch.fcluster(dist,cluster,criterion='distance') # or distance avec un seuille de différence
+    plt.figure()
+    sch.dendrogram(dist)
+    plt.show()
+    
     columns = [df.columns.tolist()[i] for i in list(np.argsort(label))]
     df = df.reindex(columns, axis='columns')
     df = df.reindex(columns, axis='index')
