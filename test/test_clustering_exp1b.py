c61567d252a62c04c44f63b7872feb5482eda709
Daphilippe <58039594+Daphilippe@users.noreply.github.com>
2021-05-21 16:13:57 +0200

    210521.1613
    mean of silhouette_score  remplace by min .

--- test/test_clustering_exp1b.py
+++ test/test_clustering_exp1b.py
@@ -2,7 +2,7 @@
 """
 @author: Duy Anh Philippe Pham
 @date: 21/05/2021
-@version: 1.00
+@version: 1.75
 @Recommandation: Python 3.7
 @But: KMedoids : déterminer le meilleur k
 """
@@ -24,38 +24,35 @@ index=np.load('../variables/'+hemi+'/matrix_index.npy',allow_pickle=True)
 columns=np.load('../variables/'+hemi+'/matrix_columns.npy',allow_pickle=True)
 data=pd.DataFrame((d>0.01)*d,index=index,columns=columns)#0 sur la diagonale, probleme de virgule flotante résolue
 
-itermax=20
-cluster=list(range(2,np.shape(d)[0]))
+itermax=1000
+cluster=list(range(2,30))
 labels=[]
-score=[]
-for j in range(itermax):
+scores=[]
+
+for i in cluster:
     label=[]
-    for i in cluster:
-        kmedoids=KMedoids(n_clusters=i  ,metric='precomputed',init='k-medoids++',max_iter=10000).fit(data)
+    score=[]
+    for j in range(itermax):
+        kmedoids=KMedoids(n_clusters=i,metric='precomputed',init='k-medoids++',max_iter=1000).fit(data)
         label.append(kmedoids.labels_)#label pour un cluster donnée
-        if j==0:# first iteration
-            score.append(silhouette_score(data,kmedoids.labels_,metric='precomputed'))
-        else:
-            score[i-2]=score[i-2]+silhouette_score(data,kmedoids.labels_,metric='precomputed')
-    labels.append(label)#labels de l'ensemble des clusters par tirage
+        score.append(silhouette_score(data,kmedoids.labels_,metric='precomputed'))
+    labels.append(label)#labels de l'ensemble des tirages pour un cluster 
+    scores.append(np.min(score))
 
 score=[i/itermax for i in score]
-label=[]
-for i in cluster:  
-    label.append([k[i-2] for k in labels])#label pour un cluster par tirage
     
 plt.figure()
-plt.plot(cluster, score, 'r+')
+plt.plot(cluster, scores, 'r+')
 plt.title('Silhouette evolution according to the number of clusters')
 plt.show()
 
 if False:        
     #Calcul des labels par vote majoritaire à améliorer si permutation de label équivalent d'une série à l'autre
     Labels_majority=[] #label après vote majoritaire pour chaque cluster
-    for i in label:
+    for i in labels:
         majorityvote=[]
         for j in range(np.shape(d)[0]):
-            majorityvote.append(Counter([k[j] for k in i]).most_common()[0][0])#label pour un cluster par tirage
+            majorityvote.append(Counter([k[j] for k in i]).most_common()[0][0])#label par majority vote pour chaque cluster
         Labels_majority.append(majorityvote)
 
 
@@ -74,7 +71,7 @@ if False:
             break
 if True:
     clus=12
-    for l in label[clus-2]:
+    for l in labels[clus-2]:
         df=data.copy()
         columns = [df.columns.tolist()[i] for i in list(np.argsort(l))]
         df = df.reindex(columns, axis='columns')
