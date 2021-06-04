29bb926e67870de2e7765f60d4585de607420084
Daphilippe <58039594+Daphilippe@users.noreply.github.com>
2021-06-04 12:03:24 +0200

    040621.1203

--- test/test_isomap_exp1.py
+++ test/test_isomap_exp1.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 @author: Duy Anh Philippe Pham
-@date: 27/05/2021
+@date: 02/06/2021
 @version: 1.25
 @Recommandation: Python 3.7
 @revision 03/06/21
@@ -30,31 +30,30 @@ if not(all(columns==index)):#controle intégrité matrice distance
 d=np.load('../variables/'+hemi+'/matrix.npy')
 data=pd.DataFrame((d>0.01)*np.sqrt(d),index=index,columns=columns)
 
-clus=4
+clus=2
 cluster=np.load(source2+'/labels.npy')[clus-2]#labels des différents clusters
 
 if False:# Isomap 1 dimension, 4 voisins
-    iso=Isomap(n_neighbors=4,n_components=1,metric='precomputed')
+    iso=Isomap(n_neighbors=15,n_components=1,metric='precomputed')
     X_transformed=iso.fit_transform(data) 
     
-    plt.figure(figsize=(20,20))
-    plt.scatter([i[0] for i in X_transformed],[i[0] for i in X_transformed],marker='*',c=cluster)
+    plt.figure(figsize=(10,3))
+    plt.scatter([i[0] for i in X_transformed],np.zeros(len(X_transformed)),marker='*',c=cluster)
+    plt.ylim(-1,1)
     j=0
-    for i in list(zip(index,X_transformed)):
-        j=j+0.1
-        plt.text(i[1]+j,i[1],i[0],fontsize=8)
-    #plt.legend()
+    for i in np.argsort(X_transformed,0):
+        j=j+1
+        plt.text(X_transformed[i]-0.2,(np.mod(j,10)-5)/10,int(i),fontsize=10)
     plt.show()
     
 if False:# Isomap 2 dimension, 3 voisins
-    iso=Isomap(n_neighbors=3,n_components=2,metric='precomputed')
+    iso=Isomap(n_neighbors=15,n_components=2,metric='precomputed')
     X_transformed=iso.fit_transform(data)
     
     plt.figure(figsize=(10,10))
     plt.scatter([i[0] for i in X_transformed],[i[1] for i in X_transformed],marker='+',c=cluster)
-    for i in list(zip(index,X_transformed)):
-        plt.annotate(i[0], i[1],fontsize=8)
-    #plt.legend()
+    for i in enumerate(X_transformed):
+        plt.annotate(i[0],i[1],fontsize=10)
     plt.show()
     
     
@@ -65,16 +64,15 @@ def animate(frame):
   plt.pause(.001)
   return frame
 
-    
 if True:# Isomap 3 dimension, 2 voisins
-    iso=Isomap(n_neighbors=2,n_components=3,metric='precomputed')
+    iso=Isomap(n_neighbors=3,n_components=3,metric='precomputed')
     X_transformed=iso.fit_transform(data)
     
     fig=plt.figure(figsize=(10,10))
     ax = plt.axes(projection='3d')
     ax.scatter3D([i[0] for i in X_transformed],[i[1] for i in X_transformed],[i[2] for i in X_transformed],marker='+',c=cluster)
-    if False:
-        for i in list(zip(index,X_transformed)):
+    if True:
+        for i in enumerate(X_transformed):
             ax.text(i[1][0],i[1][1],i[1][2],'%s' % (str(i[0])),fontsize=7)    
-    anim = animation.FuncAnimation(fig, animate, frames=120, interval=120)
+    #anim = animation.FuncAnimation(fig, animate, frames=120, interval=120)
     
--- test/test_isomap_exp2.py
+++ test/test_isomap_exp2.py
@@ -4,8 +4,8 @@
 @date: 03/06/21
 @version: 1.00
 @Recommandation: Python 3.7
-@revision 
-@But : Afficher les données en respectant la géodésie des données
+@revision 04/06/21
+@But : Cluster Kmedoids sur isomap
 """
 import numpy as np
 import sys
@@ -31,46 +31,46 @@ d=np.load('../variables/'+hemi+'/matrix.npy')
 data=pd.DataFrame((d>0.01)*np.sqrt(d),index=index,columns=columns)
 
 clus=2
-if False:# Isomap 1 dimension, 2 voisins
+if True:# Isomap 1 dimension, 2 voisins
     for cluster in np.load(source2+'/labels.npy'):
-        iso=Isomap(n_neighbors=5,n_components=1,metric='precomputed')
+        iso=Isomap(n_neighbors=15,n_components=1,metric='precomputed')
         X_transformed=iso.fit_transform(data) 
         
-        plt.figure(figsize=(20,20))
-        plt.scatter([i[0] for i in X_transformed],[i[0] for i in X_transformed],marker='*',c=cluster)
+        plt.figure(figsize=(10,3))
+        plt.scatter([i[0] for i in X_transformed],np.zeros(len(X_transformed)),marker='*',c=cluster)
+        plt.ylim(-1,1)
         j=0
-        for i in list(zip(index,X_transformed)):
-            j=j+0.1
-            plt.text(i[1]+j,i[1],i[0],fontsize=8)
-            plt.title('Isomap dim 1 - '+str(clus)+' clusters')
-        tools.save_fig(clus,source2+'/isomap_dim1_5vois/')
+        for i in np.argsort(X_transformed,0):
+            j=j+1
+            plt.text(X_transformed[i]-0.2,(np.mod(j,10)-5)/10,int(i),fontsize=10)
+        tools.save_fig(clus,source2+'/isomap/dim1/')
         clus=clus+1
 clus=2    
-if False:# Isomap 2 dimension    
+if True:# Isomap 2 dimension    
     for cluster in np.load(source2+'/labels.npy'):
-        iso=Isomap(n_neighbors=5,n_components=2,metric='precomputed')
+        iso=Isomap(n_neighbors=15,n_components=2,metric='precomputed')
         X_transformed=iso.fit_transform(data) 
         
         plt.figure(figsize=(10,10))
         plt.scatter([i[0] for i in X_transformed],[i[1] for i in X_transformed],marker='+',c=cluster)
-        for i in list(zip(index,X_transformed)):
-            plt.annotate(i[0], i[1],fontsize=8)
+        for i in enumerate(X_transformed):
+            plt.annotate(i[0],i[1],fontsize=10)
             
         plt.title('Isomap dim 2 - '+str(clus)+' clusters')
-        tools.save_fig(clus,source2+'/isomap_dim2_5vois/')
+        tools.save_fig(clus,source2+'/isomap/dim2/')
         clus=clus+1
 
-if True:# Isomap 3 dimension, 2 voisins
+if False:# Isomap 3 dimension, 2 voisins
     for cluster in np.load(source2+'/labels.npy'):
-        iso=Isomap(n_neighbors=2,n_components=3,metric='precomputed')
+        iso=Isomap(n_neighbors=15,n_components=3,metric='precomputed')
         X_transformed=iso.fit_transform(data) 
         
         fig=plt.figure(figsize=(10,10))
         ax = plt.axes(projection='3d')
         ax.scatter3D([i[0] for i in X_transformed],[i[1] for i in X_transformed],[i[2] for i in X_transformed],marker='+',c=cluster)
-        if False:
-            for i in list(zip(index,X_transformed)):
-                ax.text(i[1][0],i[1][1],i[1][2],'%s' % (str(i[0])),fontsize=7)    
+        if True:
+            for i in enumerate(X_transformed):
+                ax.text(i[1][0],i[1][1],i[1][2],'%s' % (str(i[0])),fontsize=7)       
         plt.show()        
     
     
