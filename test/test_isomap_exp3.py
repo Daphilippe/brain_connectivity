f5c8c68e480957954bb6bade0a5467e2af46b67f
Daphilippe <58039594+Daphilippe@users.noreply.github.com>
2021-06-04 14:49:56 +0200

    040621.1449
    A expliquer le raisonnement

--- test/test_isomap_exp3.py
+++ test/test_isomap_exp3.py
@@ -27,42 +27,36 @@ if not(all(columns==index)):#controle intégrité matrice distance
 d=np.load('../variables/'+hemi+'/matrix.npy')
 data=pd.DataFrame((d>0.01)*np.sqrt(d),index=index,columns=columns)
 
-clus=6
+clus=2
 cluster= np.load(source2+'/labels.npy')[clus-2]
-
-# corrélation intrinsèque
-if False:# Isomap 1 dimension, 2 voisins
-    temp=[]
-    voisins=range(2,25)
-    for k in voisins:
-        iso=Isomap(n_neighbors=k,n_components=1,metric='precomputed')
-        X_transformed=iso.fit_transform(data)   
-        [a]=np.argsort(X_transformed,0).T
-        temp.append(a.T)     
-    print(np.sum(np.sum(pd.DataFrame(np.array(temp)).T.corr())/len(voisins))/len(voisins))#corrélation moyenne entre isomap 1d
-
-
+                
 # corrélation par cluster
 if True:# 
-    temp=[]
     voisins=range(2,25)
-    axis=0
-    for k in voisins:
-        iso=Isomap(n_neighbors=k,n_components=3,metric='precomputed')
-        X_transformed=iso.fit_transform(data)  
-        
-        d={}
-        for i in range(clus):
-            d[i]=[]
-        for i in enumerate(np.argsort(X_transformed,0)):
-            d[cluster[i[0]]].append(i[1][axis])# on observe selon une composante
-        for i in d:
-            d[i]=np.sort(d[i])
-        temp.append(d)
-    a=pd.DataFrame.from_dict(temp)
-    
-    for i in a:
+    n_components=3
+    view=[]
+    for k in voisins:# on change le nombre de voisin
         temp=[]
-        for j in a[i]:
-            temp.append(j)
-        print(np.min(np.min(pd.DataFrame(temp).T.corr())))# la plus petite corrélation
+        for axis in range(n_components):
+            iso=Isomap(n_neighbors=k,n_components=n_components,metric='precomputed')
+            X_transformed=iso.fit_transform(data)  
+            
+            d={}
+            for i in range(clus):# regroupement des points par cluster
+                d[i]=[]
+            for i in enumerate(np.argsort(X_transformed,0)):#récupération de l'ordre établit par l'isomap en fonction de l'axe
+                d[cluster[i[0]]].append(i[1][axis])# on observe selon une composante
+            for i in d:#trie par cluster de l'ordre des points dans l'isomap selon l'axe 
+                d[i]=np.sort(d[i])
+            temp.append(d)
+        a=pd.DataFrame.from_dict(temp)
+        
+        # reconditionnement des données pour calcul de la corrélation par cluster
+        temp2=[]
+        for i in a:
+            temp=[]
+            for j in a[i]:
+                temp.append(j)
+            temp2.append(np.min(np.min(pd.DataFrame(temp).T.corr())))# la plus petite corrélation sur les n axes
+        view.append(temp2)
+    print(np.min(view))# on a la corrélation minimal sur notre jeu de donnée
