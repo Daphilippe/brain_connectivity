3529509b85722aa1460e23b02b84d5547dd321f7
Daphilippe <58039594+Daphilippe@users.noreply.github.com>
2021-06-14 15:53:38 +0200

    140621.1553

---
+++ test/Isomap/test_barycentre_glissant_exp1.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Duy Anh Philippe Pham
+@date: 11/06/2021
+@version: 1.20
+@Recommandation: Python 3.7
+@révision:14/06/21
+@But : Barycentre glissant
+"""
+import numpy as np
+import sys
+sys.path.insert(1,'../libs')
+
+import pandas as pd
+from  sklearn.manifold import Isomap
+import matplotlib.pylab as plt
+
+import tools, display, barycenter, process
+
+# Directory
+hemi='L'
+source='../../data/'+hemi+'/'
+source2="../../variables/clustering/"+hemi
+source3='../../variables/'
+source4='../../variables/isomap/'+hemi+'/barycentre glissant/'
+
+columns=np.load(source3+hemi+'/matrix_columns.npy',allow_pickle=True)
+index=np.load(source3+hemi+'/matrix_index.npy',allow_pickle=True)
+if not(all(columns==index)):#controle intégrité matrice distance
+    print('erreur')
+    
+d=np.load(source3+hemi+'/matrix.npy')
+data=pd.DataFrame((d>0.01)*np.sqrt(d),index=index,columns=columns)
+
+if True:# Isomap 1 dimension, 2 voisins
+    bary=[]
+    iso=Isomap(n_neighbors=10,n_components=1,metric='precomputed')
+    X_transformed=iso.fit_transform(data) 
+    trie=np.argsort(X_transformed,0)# sujets classés selon l'axe de projection
+    
+    #barycentre
+    ### Paramètres du barycentre
+    k=500
+    X_init = np.random.normal(0., 1., (k, 2))  # initial Dirac locations
+    b=np.ones(k)*1/k
+    X=None
+    
+    ### Paramètre pour l'intervalle du barycentre glissant
+    inter=21
+    index_bis=index[trie]
+    
+    for i in range(len(index_bis)-inter):
+        measures_locations=[]
+        measures_weights=[]
+        for j in index_bis[i:i+inter]:
+            measures_locations.append(np.load(source+str(j[0])+'_connectivity_withHKnob.npy'))
+            measures_weights.append(np.ones( np.shape(measures_locations[-1])[0] )*1/np.shape(measures_locations[-1])[0])
+        bary.append(barycenter.iterative_barycenter(X,X_init,b,measures_locations,measures_weights,Nmax=inter,save=False))   
+    tools.save_value(bary,'bary_glissant_'+str(inter)+'_'+hemi,source4+str(inter))    
--- test/Isomap/test_barycentre_glissant_exp2.py
+++ test/Isomap/test_barycentre_glissant_exp2.py
@@ -2,8 +2,9 @@
 """
 @author: Duy Anh Philippe Pham
 @date: 11/06/2021
-@version: 1.00
+@version: 1.20
 @Recommandation: Python 3.7
+@révision:14/06/21
 @But : Barycentre glissant
 """
 import numpy as np
@@ -42,26 +43,30 @@ trie=np.argsort(X_transformed,0)
 min_distance=1
 percent=90#seuillage des valeurs
 grid_size=101# valeur par défaut
-
+liste=[]
 if True:
     for i in enumerate(bary):
         text='Barycenter of:'
         for j in index[trie[i[0]:i[0]+inter]]:
             text+=' '+str(j[0])
         _,_,img_xs=tools.estimate_pseudo_density(i[1])
-        data=img_xs/np.max(img_xs)
-        
-        coord = peak_local_max((data>np.percentile(data, percent))*data, min_distance)
-        plt.figure()
-        extent = (0,grid_size-1 , 0,grid_size-1)
-        plt.imshow(data,cmap=plt.cm.magma_r,origin='lower',extent=extent)
-        plt.autoscale(False)
-        plt.plot(coord[:, 1], coord[:, 0], 'g.')
-        plt.axis('on')
-        plt.xlabel('Precentral gyral crest scaled to 100')
-        plt.ylabel('Post central gyral crest scaled to 100')
-        plt.colorbar()
-        plt.grid(linestyle = '--', linewidth = 0.5,alpha=0.5, which='major')
-        plt.title('Central subject n°'+str(i[0]+int(inter/2)))
-        tools.save_fig(str(i[0]),source2+str(inter)+'/')
-        #tools.save_value(coord, str(i[0]),source2+'/'+str(inter)+'/')
+        #data=img_xs/np.max(img_xs)
+        liste.append(img_xs)
+liste=liste/np.max(liste)
+if True:
+    for data in liste:
+            coord = peak_local_max((data>np.percentile(data, percent))*data, min_distance)
+            plt.figure()
+            extent = (0,grid_size-1 , 0,grid_size-1)
+            plt.imshow(data,cmap=plt.cm.magma_r,origin='lower',extent=extent)
+            plt.autoscale(False)
+            plt.plot(coord[:, 1], coord[:, 0], 'g.')
+            plt.axis('on')
+            plt.xlabel('Precentral gyral crest scaled to 100')
+            plt.ylabel('Post central gyral crest scaled to 100')
+            plt.colorbar()
+            plt.clim(0,1)
+            plt.grid(linestyle = '--', linewidth = 0.5,alpha=0.5, which='major')
+            plt.title('Central subject n°'+str(i[0]+int(inter/2)))
+            tools.save_fig(str(i[0]),source2+str(inter)+'/')
+            #tools.save_value(coord, str(i[0]),source2+'/'+str(inter)+'/')
